ec645010e9e5c9e0bd042e8d423b9092560d4dfd
Tate House <tatemonroehouse@gmail.com>
2024-03-31 19:29:07 -0400

    Feature: Unsubscribe link spam filter
    The unsubscribe link spam filter checks if an email contains an
    unsubscribe link. If it does not contain an unsubscribe link, it will be
    flagged as spam.

---
+++ EmailSpamFilter.Core/Filters/UnsubscribeLinkSpamFilter.cs
@@ -0,0 +1,21 @@
+﻿namespace EmailSpamFilter.Core.Filters;
+using EmailSpamFilter.Core.Models;
+using EmailSpamFilter.Core.Utilities;
+
+public class UnsubscribeLinkSpamFilter : ISpamFilter
+{
+	private readonly ILinkExtractor linkExtractor;
+
+	public UnsubscribeLinkSpamFilter(ILinkExtractor linkExtractor)
+	{
+		this.linkExtractor = linkExtractor ?? throw new ArgumentNullException(nameof(linkExtractor));
+	}
+
+	public Task<bool> IsSpamAsync(Email email)
+	{
+		var links = linkExtractor.ExtractLinks(email.Body);
+		var unsubscribeLinks = links.Any(link => link.ToLowerInvariant().Contains("unsubscribe"));
+
+		return Task.FromResult(!unsubscribeLinks);
+	}
+}
