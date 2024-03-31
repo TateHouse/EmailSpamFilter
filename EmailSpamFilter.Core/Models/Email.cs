471a2646296c6fd7286d57c02b335dad9f31feaa
Tate House <tatemonroehouse@gmail.com>
2024-03-31 15:13:53 -0400

    Chore: Email model
    An email will be stored as two separate components, the subject and the
    body.

---
+++ EmailSpamFilter.Core/Models/Email.cs
@@ -0,0 +1,12 @@
+﻿namespace EmailSpamFilter.Core.Models;
+public class Email
+{
+	public string Subject { get; private set; }
+	public string Body { get; private set; }
+
+	public Email(string subject, string body)
+	{
+		Subject = subject;
+		Body = body;
+	}
+}
