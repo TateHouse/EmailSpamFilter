57a43c571b56d75678258cd60eb08e94a9b02dbb
Tate House <tatemonroehouse@gmail.com>
2024-03-31 18:15:33 -0400

    Feature: Link analysis spam filter
    The link analysis spam filter uses the Google Safe Browsing API to
    determine if links in the email are potentially malicious.

---
+++ EmailSpamFilter.Core.Test/Filters/LinkAnalysisSpamFilterTest.cs
@@ -0,0 +1,36 @@
+﻿namespace EmailSpamFilter.Core.Test.Filters;
+using EmailSpamFilter.Core.Filters;
+using EmailSpamFilter.Core.Models;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+
+[TestFixture]
+public class LinkAnalysisSpamFilterTest
+{
+	private ISpamFilter spamFilter;
+
+	[SetUp]
+	public void SetUp()
+	{
+		var configuration = new ConfigurationBuilder().AddUserSecrets<LinkAnalysisSpamFilter>().Build();
+		spamFilter = new LinkAnalysisSpamFilter(configuration);
+	}
+
+	[Test]
+	public async Task GivenEmailWithUnsafeLink_WhenIsSpamAsync_ThenReturnsTrue()
+	{
+		var email = new Email("Subject", "http://malware.testing.google.test/testing/malware/");
+		var result = await spamFilter.IsSpamAsync(email);
+
+		result.Should().BeTrue();
+	}
+
+	[Test]
+	public async Task GivenEmailWithSafeLink_WhenIsSpamAsync_ThenReturnsFalse()
+	{
+		var email = new Email("Subject", "https://www.google.com");
+		var result = await spamFilter.IsSpamAsync(email);
+
+		result.Should().BeFalse();
+	}
+}
---
+++ EmailSpamFilter.Core/Filters/LinkAnalysisSpamFilter.cs
@@ -0,0 +1,89 @@
+﻿namespace EmailSpamFilter.Core.Filters;
+using System.Text.RegularExpressions;
+using EmailSpamFilter.Core.Models;
+using Google.Apis.Safebrowsing.v4;
+using Google.Apis.Safebrowsing.v4.Data;
+using Google.Apis.Services;
+using Microsoft.Extensions.Configuration;
+
+public class LinkAnalysisSpamFilter : ISpamFilter
+{
+	private readonly SafebrowsingService safebrowsingService;
+
+	public LinkAnalysisSpamFilter(IConfiguration configuration)
+	{
+		if (string.IsNullOrWhiteSpace(configuration["googleApiKey"]))
+		{
+			throw new ArgumentException("Google API key is missing");
+		}
+
+		var googleApiKey = configuration["googleApiKey"];
+		safebrowsingService = new SafebrowsingService(new BaseClientService.Initializer
+		{
+			ApiKey = googleApiKey
+		});
+	}
+
+	public async Task<bool> IsSpamAsync(Email email)
+	{
+		var links = ExtractLinks(email.Body);
+
+		foreach (var link in links)
+		{
+			var isUnsafe = await IsLinkUnsafe(link);
+
+			if (isUnsafe)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private static IEnumerable<string> ExtractLinks(string text)
+	{
+		const string pattern = @"^(https:|http:|www\.)\S*";
+		var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+		return regex.Matches(text).Select(match => match.Value).ToList();
+	}
+
+	private async Task<bool> IsLinkUnsafe(string link)
+	{
+		var threatMatchesRequest = new GoogleSecuritySafebrowsingV4FindThreatMatchesRequest
+		{
+			Client = new GoogleSecuritySafebrowsingV4ClientInfo
+			{
+				ClientId = "EmailSpamFilter",
+				ClientVersion = "1.0.0"
+			},
+			ThreatInfo = new GoogleSecuritySafebrowsingV4ThreatInfo
+			{
+				ThreatTypes = new List<string>
+				{
+					"MALWARE",
+					"SOCIAL_ENGINEERING",
+					"UNWANTED_SOFTWARE",
+					"POTENTIALLY_HARMFUL_APPLICATION"
+				},
+				PlatformTypes = new List<string>
+				{
+					"WINDOWS"
+				},
+				ThreatEntryTypes = new List<string>
+				{
+					"URL"
+				},
+				ThreatEntries = new List<GoogleSecuritySafebrowsingV4ThreatEntry>
+				{
+					new GoogleSecuritySafebrowsingV4ThreatEntry { Url = link }
+				}
+			}
+		};
+
+		var threatMatches = await safebrowsingService.ThreatMatches.Find(threatMatchesRequest).ExecuteAsync();
+
+		return threatMatches?.Matches?.Count > 0;
+	}
+}
