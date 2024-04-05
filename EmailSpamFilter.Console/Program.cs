6c76cd629eeaa5fbf85f5d39d0fcae55f3cb3d32
Tate House <tatemonroehouse@gmail.com>
2024-04-05 11:06:02 -0400

    Chore: Configure AppSettings.json

--- EmailSpamFilter.Console/Program.cs
+++ EmailSpamFilter.Console/Program.cs
@@ -1,8 +1,12 @@
 ﻿namespace EmailSpamFilter.Console;
+using Microsoft.Extensions.Configuration;
+
 public static class Program
 {
 	public static void Main(string[] args)
 	{
-
-	}
+		var configurationBuilder = new ConfigurationBuilder();
+		configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+		configurationBuilder.AddJsonFile("AppSettings.json", false, true);
+		var configuration = configurationBuilder.Build();
 }
